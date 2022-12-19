54a8ef568e5f5f5786ec6319f76a369e097b6d40
hoseinmoradi <moradih7379@gmail.com>
2022-12-19 08:53:33 +0330

    completed scroll mode on wrist watch for mobile size.

--- src/components/main/wrist-watch/WristWatch.jsx
+++ src/components/main/wrist-watch/WristWatch.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./wristWatch.css";
-import  wrist1  from "../../../assets/image/wrist3.jpg";
+import wrist1 from "../../../assets/image/wrist2.jpg";
 
 const WristWatch = () => {
   return (
@@ -21,6 +21,69 @@ const WristWatch = () => {
       <main className="watch__slider__wrapper">
         <span className="prev__slider">P</span>
         <section className="box__slider">
+          <div className="slider__card">
+            <img alt="watch" src={wrist1} />
+            <div className="card__content">
+              <h6 className="card__brand">KALVIN KLEIN</h6>
+              <p className="card__detail">ساعت مردانه Calvin Klein 25200065</p>
+              <p className="card__price">2,000,000 تومان</p>
+              <button className="card__cta">افزودن به سبد خرید</button>
+            </div>
+          </div>
+          <div className="slider__card">
+            <img alt="watch" src={wrist1} />
+            <div className="card__content">
+              <h6 className="card__brand">KALVIN KLEIN</h6>
+              <p className="card__detail">ساعت مردانه Calvin Klein 25200065</p>
+              <p className="card__price">2,000,000 تومان</p>
+              <button className="card__cta">افزودن به سبد خرید</button>
+            </div>
+          </div>
+          <div className="slider__card">
+            <img alt="watch" src={wrist1} />
+            <div className="card__content">
+              <h6 className="card__brand">KALVIN KLEIN</h6>
+              <p className="card__detail">ساعت مردانه Calvin Klein 25200065</p>
+              <p className="card__price">2,000,000 تومان</p>
+              <button className="card__cta">افزودن به سبد خرید</button>
+            </div>
+          </div>
+          <div className="slider__card">
+            <img alt="watch" src={wrist1} />
+            <div className="card__content">
+              <h6 className="card__brand">KALVIN KLEIN</h6>
+              <p className="card__detail">ساعت مردانه Calvin Klein 25200065</p>
+              <p className="card__price">2,000,000 تومان</p>
+              <button className="card__cta">افزودن به سبد خرید</button>
+            </div>
+          </div>
+          <div className="slider__card">
+            <img alt="watch" src={wrist1} />
+            <div className="card__content">
+              <h6 className="card__brand">KALVIN KLEIN</h6>
+              <p className="card__detail">ساعت مردانه Calvin Klein 25200065</p>
+              <p className="card__price">2,000,000 تومان</p>
+              <button className="card__cta">افزودن به سبد خرید</button>
+            </div>
+          </div>
+          <div className="slider__card">
+            <img alt="watch" src={wrist1} />
+            <div className="card__content">
+              <h6 className="card__brand">KALVIN KLEIN</h6>
+              <p className="card__detail">ساعت مردانه Calvin Klein 25200065</p>
+              <p className="card__price">2,000,000 تومان</p>
+              <button className="card__cta">افزودن به سبد خرید</button>
+            </div>
+          </div>
+          <div className="slider__card">
+            <img alt="watch" src={wrist1} />
+            <div className="card__content">
+              <h6 className="card__brand">KALVIN KLEIN</h6>
+              <p className="card__detail">ساعت مردانه Calvin Klein 25200065</p>
+              <p className="card__price">2,000,000 تومان</p>
+              <button className="card__cta">افزودن به سبد خرید</button>
+            </div>
+          </div>
           <div className="slider__card">
             <img alt="watch" src={wrist1} />
             <div className="card__content">
